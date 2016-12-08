6b6b6a73b07ad6ad425b96182b8a77be1b0d2b20
谢毅 <179806605@qq.com>
2016-12-08 11:26:09 +0800

    初始化程序

---
+++ first-demo/first-demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  first-demo
+//
+//  Created by 谢毅 on 16/12/8.
+//  Copyright © 2016年 xieyi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
